17ce786410a4c329cfe6145241d0525d23b2a424
sujalsinghal25 <singhalsujal25@gmail.com>
2026-07-06 14:10:59 +0000

    Update app message for demo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline!');
+  res.send('Hello from CI/CD Pipeline! - Automated Deployment v2');
 });
 
 app.listen(PORT, () => {
