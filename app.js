10c276fabcb18a08b5a43aaa9ca2084f6265394f
sujalsinghal25 <singhalsujal25@gmail.com>
2026-07-12 17:14:53 +0000

    Demo: automated deployment v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! - Automated Deployment V2');
+  res.send('Hello from CI/CD Pipeline! - Automated Deployment v2');
 });
 
 app.listen(PORT, () => {
