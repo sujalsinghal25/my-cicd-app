0926d17af45790df2592e519c1cfe974ab0d7257
sujalsinghal25 <singhalsujal25@gmail.com>
2026-07-17 13:25:06 +0000

    hello

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Engineer! - Automated Deployment v7');
+  res.send('Hello from DevOps Engineer Sujal Singhal! - Automated Deployment v7');
 });
 
 app.get('/status', (req, res) => {
