d8dc6a49751cc0baf5bf0f148130c8e088a96f72
sujalsinghal25 <singhalsujal25@gmail.com>
2026-07-20 09:14:59 +0000

    updated repo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Engineer Sujal Singhal! - Automated Deployment v7');
+  res.send('Hello from Sujal Singhal! - Automated Deployment v8');
 });
 
 app.get('/status', (req, res) => {
